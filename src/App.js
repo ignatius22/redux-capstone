5fc3e10781f4abfbc1f44a754b160e61b8dc9ba4
Ignatius22 <igee54077@gmail.com>
2020-11-16 08:05:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <button onClick={handleClick}>change name</button>
+    </div>
+  );
+}
+
+export default App;
