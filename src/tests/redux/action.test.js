42f5661fab2bad47edc9d63dc82401277171083d
ignatius22 <igee54077@gmail.com>
2020-11-24 10:46:54 +0100

    make test pass without errors

---
+++ src/tests/redux/action.test.js
@@ -0,0 +1,42 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { setSearchField} from '../../redux/action';
+
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+
+it('should dispatch action', () => {
+  const initialState = {};
+  const store = mockStore(initialState);
+
+  store.dispatch(setSearchField());
+
+  const actions = store.getActions();
+  const expectedPayload = { type: 'CHANGE_SEARCHFIELD' };
+  expect(actions).toEqual([expectedPayload]);
+});
+
+function success() {
+  return {
+    type: 'REQUEST_RECIPES_SUCCESS',
+  };
+}
+
+function fetchData() {
+  return (dispatch) => {
+    return fetch(
+      'https://api.edamam.com/search?q=chicken&app_id=4e80d906&app_key=90d9ed76fe22d477f1c7c3676ed9f686&from=0&to=20'
+    )
+      .then(() => dispatch(success()));
+  };
+}
+
+it('should execute fetch data', () => {
+  const store = mockStore({});
+  return store.dispatch(fetchData()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual(success());
+  });
+});
